a399e31f8db800049c4e9623be307809c290d805
Renat Akhmerov <rakhmerov@mirantis.com>
2013-11-19 13:35:06 +0700

    Fixing licence in setyp.py
    Change-Id: I3d29d47d851b23136c736550c0a03c5e32f89a42

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+# Copyright (c) 2013 Mirantis Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
