49643c4ae8f1760c727f1bd91088fd88e559369b
Alexander Kuznetsov <akuznetsov@mirantis.com>
2013-12-10 12:39:51 +0400

    Added keystone token authorization
    Change-Id: I585dfd3e2ece26dab7f18d1117cad0dca0baf72b

--- mistralclient/api/httpclient.py
+++ mistralclient/api/httpclient.py
@@ -18,38 +18,45 @@ import requests
 
 
 class HTTPClient(object):
-    def __init__(self, base_url, token):
+    def __init__(self, base_url, token, project_id, user_id):
         self.base_url = base_url
         self.token = token
+        self.project_id = project_id
+        self.user_id = user_id
 
     def get(self, url, headers=None):
-        if not headers:
-            headers = {}
-
-        headers['x-auth-token'] = self.token
+        headers = self._update_headers(headers)
 
         return requests.get(self.base_url + url, headers=headers)
 
     def post(self, url, body, headers=None):
-        if not headers:
-            headers = {'content-type': 'application/json'}
-
-        headers['x-auth-token'] = self.token
+        headers = self._update_headers(headers)
+        content_type = headers.get('content-type', 'application/json')
+        headers['content-type'] = content_type
 
         return requests.post(self.base_url + url, body, headers=headers)
 
     def put(self, url, body, headers=None):
-        if not headers:
-            headers = {'content-type': 'application/json'}
-
-        headers['x-auth-token'] = self.token
+        headers = self._update_headers(headers)
+        content_type = headers.get('content-type', 'application/json')
+        headers['content-type'] = content_type
 
         return requests.put(self.base_url + url, body, headers=headers)
 
     def delete(self, url, headers=None):
-        if not headers:
-            headers = {}
-
-        headers['x-auth-token'] = self.token
+        headers = self._update_headers(headers)
 
         return requests.delete(self.base_url + url, headers=headers)
+
+    def _update_headers(self, headers):
+        if not headers:
+            headers = {}
+        token = headers.get('x-auth-token', self.token)
+        headers['x-auth-token'] = token
+
+        project_id = headers.get('X-Project-Id', self.project_id)
+        headers['X-Project-Id'] = project_id
+
+        user_id = headers.get('X-User-Id', self.user_id)
+        headers['X-User-Id'] = user_id
+        return headers
