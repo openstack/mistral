b2d2dc554fa6f841dbf2c890cc95b50d968d67b6
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2014-05-12 19:29:39 +0400

    Fix creating trust on workbook creation
     * We used the wrong project name for creating the trust. 
       Now we are using project name from config
    
    Closes bug: #1318603
    
    Change-Id: I51d1da115b0e0e9551d0174c821ed2886f5a79d8

--- mistral/services/trusts.py
+++ mistral/services/trusts.py
@@ -29,7 +29,8 @@ def create_trust(workbook):
 
     ctx = context.ctx()
 
-    trustee_id = keystone.client_for_admin(ctx['project_id']).user_id
+    trustee_id = keystone.client_for_admin(
+        CONF.keystone.admin_tenant_name).user_id
 
     trust = client.trusts.create(trustor_user=client.user_id,
                                  trustee_user=trustee_id,
