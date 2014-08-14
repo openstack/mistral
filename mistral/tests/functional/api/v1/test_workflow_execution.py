ac97745db7b0e24af28be4a71409d1c735948fee
Anastasia Kuznetsova <akuznetsova@mirantis.com>
2014-08-14 15:42:04 +0400

    Add tests which check task dependencies
    - Added new test file with tests which check workflow execution and
    task dependencies.
    - Update base.py file, rename '_create_execution' method, make it more
    common
    - Refactor tests in the test_mistral_base file according method remaning
    - Change test workflow demo.yaml
    
    Targets blueprint: mistral-workflow-execution-tests
    
    Change-Id: If58d9d0da8a7204214f92fdb12ed87cb1801c17b

---
+++ mistral/tests/functional/api/v1/test_workflow_execution.py
@@ -0,0 +1,147 @@
+# Copyright 2013 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+from mistral.tests.functional.api import base
+
+
+CONTEXT = {
+    'person': {
+        'first_name': 'John',
+        'last_name': 'Doe',
+    }
+}
+
+
+class MistralWorkflowExecutionTests(base.TestCaseAdvanced):
+
+    def test_reverse_flow(self):
+        self.client.upload_workbook_definition(
+            'test',
+            'resources/data_flow/task_with_diamond_dependencies.yaml')
+
+        ex = self.client.create_execution_wait_success(
+            'test', CONTEXT, 'send_greeting', 150)[1]
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_full_name')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_full_name']['string'],
+                         "John Doe")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_address')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_address']['string'],
+                         "To John Doe")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_greeting')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_greeting']['string'],
+                         "Dear John Doe")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'send_greeting')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['send_greeting']['string'],
+                         "To John Doe. Dear John Doe,..")
+
+    def test_task_with_two_dependencies(self):
+        self.client.upload_workbook_definition(
+            'test', 'resources/data_flow/task_with_two_dependencies.yaml')
+
+        ex = self.client.create_execution_wait_success(
+            'test', CONTEXT, 'send_greeting', 150)[1]
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_full_name')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_full_name']['full_name'],
+                         "John Doe")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_greeting')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_greeting']['greeting'],
+                         "Cheers!")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'send_greeting')
+        task = json.loads(task['output'])
+        self.assertTrue(task['task']['send_greeting']['greeting_sent'])
+
+    def test_direct_flow_tasks_on_success(self):
+        self.client.upload_workbook_definition(
+            'test', 'resources/data_flow/three_subsequent_tasks.yaml')
+
+        ex = self.client.create_execution_wait_success(
+            'test', CONTEXT, 'build_full_name', 150)[1]
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_full_name')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_full_name']['full_name'],
+                         "John Doe")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_greeting')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_greeting']['greeting'],
+                         "Hello, John Doe!")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'send_greeting')
+        task = json.loads(task['output'])
+        self.assertTrue(task['task']['send_greeting']['greeting_sent'])
+
+    def test_two_dependent_tasks(self):
+        self.client.upload_workbook_definition(
+            'test', 'resources/data_flow/two_dependent_tasks.yaml')
+
+        ex = self.client.create_execution_wait_success(
+            'test', CONTEXT, 'build_greeting', 150)[1]
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_full_name')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_full_name']['full_name'],
+                         "John Doe")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_greeting')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_greeting']['greeting'],
+                         "Hello, John Doe!")
+
+    def test_two_subsequent_tasks(self):
+        self.client.upload_workbook_definition(
+            'test', 'resources/data_flow/two_subsequent_tasks.yaml')
+
+        ex = self.client.create_execution_wait_success(
+            'test', CONTEXT, 'build_full_name', 150)[1]
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_full_name')
+        task = json.loads(task['output'])
+        self.assertEqual(task['task']['build_full_name']['full_name'],
+                         "John Doe")
+
+        task = self.client.get_task_by_name(
+            'test', ex['id'], 'build_greeting')
+        task = json.loads(task['output'])
+        self.assertEqual(
+            task['task']['build_greeting']['greeting']['greet_message'],
+            "Hello, John Doe!")
