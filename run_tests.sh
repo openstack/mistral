9ab59e2febbe606e1426e29ab222d7835ad425ee
Lingxian Kong <anlin.kong@gmail.com>
2015-02-12 16:23:19 +0800

    Fix the wrong project name in run_tests.sh usage
    s/Neutron/Mistral
    
    Change-Id: I8202aa4f1eed4ee5fb1422fbd2ccaed18fc453f5
    Closes-Bug: #1421090

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ set -eu
 
 function usage {
   echo "Usage: $0 [OPTION]..."
-  echo "Run Neutron's test suite(s)"
+  echo "Run Mistral's test suite(s)"
   echo ""
   echo "  -V, --virtual-env           Always use virtualenv.  Install automatically if not present"
   echo "  -N, --no-virtual-env        Don't use virtualenv.  Run tests in local environment"
