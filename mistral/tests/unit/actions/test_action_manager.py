354488854397a51184f086cb6dc29b49a7f108bc
Renat Akhmerov <rakhmerov@mirantis.com>
2015-04-08 13:45:57 +0600

    Removing left v1 related stuff (resources, DSL specs)
    Change-Id: Ie85296368e326cfad1da1dfb99e84e24107051e2

---
+++ mistral/tests/unit/actions/test_action_manager.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 - Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from mistral.actions import std_actions as std
+from mistral.openstack.common import log as logging
+from mistral.services import action_manager as a_m
+from mistral.tests import base
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ActionFactoryTest(base.DbTestCase):
+    def test_register_standard_actions(self):
+        action_list = a_m.get_registered_actions()
+
+        self._assert_single_item(action_list, name="std.echo")
+        self._assert_single_item(action_list, name="std.email")
+        self._assert_single_item(action_list, name="std.http")
+        self._assert_single_item(action_list, name="std.mistral_http")
+        self._assert_single_item(action_list, name="std.ssh")
+        self._assert_single_item(action_list, name="std.javascript")
+
+        self._assert_single_item(action_list, name="nova.servers_get")
+        self._assert_single_item(action_list, name="nova.volumes_delete")
+
+        self._assert_single_item(action_list, name="keystone.users_list")
+        self._assert_single_item(action_list, name="keystone.trusts_create")
+
+        self._assert_single_item(action_list, name="glance.images_list")
+        self._assert_single_item(action_list, name="glance.images_delete")
+
+    def test_get_action_class(self):
+        self.assertEqual(std.EchoAction, a_m.get_action_class("std.echo"))
+        self.assertEqual(std.HTTPAction, a_m.get_action_class("std.http"))
+        self.assertEqual(
+            std.MistralHTTPAction,
+            a_m.get_action_class("std.mistral_http")
+        )
+        self.assertEqual(
+            std.SendEmailAction,
+            a_m.get_action_class("std.email")
+        )
+        self.assertEqual(
+            std.JavaScriptAction,
+            a_m.get_action_class("std.javascript")
+        )
