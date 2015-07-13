e523f7537c9e3dac0b4d75d42cbb2b0dc3fe2489
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2015-07-13 18:36:52 +0300

    Making / and /v2 URLs allowed without auth
    Closes-Bug: #1473963
    
    Change-Id: I7170121a1216d9d72a43e552db1864a58e4c6237

--- functionaltests/run_tests.sh
+++ functionaltests/run_tests.sh
@@ -15,7 +15,7 @@
 # How many seconds to wait for the API to be responding before giving up
 API_RESPONDING_TIMEOUT=20
 
-if ! timeout ${API_RESPONDING_TIMEOUT} sh -c "while ! curl -s http://127.0.0.1:8989/v2/ 2>/dev/null | grep -q 'Authentication required' ; do sleep 1; done"; then
+if ! timeout ${API_RESPONDING_TIMEOUT} sh -c "until curl --output /dev/null --silent --head --fail http://localhost:8989; do sleep 1; done"; then
     echo "Mistral API failed to respond within ${API_RESPONDING_TIMEOUT} seconds"
     exit 1
 fi
