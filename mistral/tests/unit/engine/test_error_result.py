84c6df1ac588027714bd6d59740f52c06cc47f4f
Renat Akhmerov <rakhmerov@mirantis.com>
2015-07-15 15:47:36 +0600

    Error result: adding more tests
    Change-Id: I60b6251a20dccdd710f8573bc09a5023086318e2

--- mistral/tests/unit/engine/test_error_result.py
+++ mistral/tests/unit/engine/test_error_result.py
@@ -20,6 +20,7 @@ from mistral.db.v2 import api as db_api
 from mistral.services import workflows as wf_service
 from mistral.tests import base as test_base
 from mistral.tests.unit.engine import base
+from mistral.workflow import data_flow
 from mistral.workflow import states
 from mistral.workflow import utils as wf_utils
 
@@ -28,6 +29,35 @@ LOG = logging.getLogger(__name__)
 # Use the set_default method to set value otherwise in certain test cases
 # the change in value is not permanent.
 cfg.CONF.set_default('auth_enable', False, group='pecan')
+
+
+WF = """
+---
+version: '2.0'
+
+wf:
+  input:
+    - success_result
+    - error_result
+
+  tasks:
+    task1:
+      action: my_action
+      input:
+        success_result: <% $.success_result %>
+        error_result: <% $.error_result %>
+      publish:
+        p_var: <% $.task1 %>
+      on-error:
+        - task2: <% $.task1 = 2 %>
+        - task3: <% $.task1 = 3 %>
+
+    task2:
+      action: std.noop
+
+    task3:
+      action: std.noop
+"""
 
 
 class MyAction(actions_base.Action):
@@ -51,35 +81,8 @@ class ErrorResultTest(base.EngineTestCase):
 
         test_base.register_action_class('my_action', MyAction)
 
-    def test_error_result(self):
-        wf_text = """---
-        version: '2.0'
-
-        wf:
-          input:
-            - success_result
-            - error_result
-
-          tasks:
-            task1:
-              action: my_action
-              input:
-                success_result: <% $.success_result %>
-                error_result: <% $.error_result %>
-              publish:
-                p_var: <% $.task1.some_field %>
-              on-error:
-                - task2: <% $.task1 = 2 %>
-                - task3: <% $.task1 = 3 %>
-
-            task2:
-              action: std.noop
-
-            task3:
-              action: std.noop
-        """
-
-        wf_service.create_workflows(wf_text)
+    def test_error_result1(self):
+        wf_service.create_workflows(WF)
 
         # Start workflow.
         wf_ex = self.engine.start_workflow(
@@ -108,3 +111,66 @@ class ErrorResultTest(base.EngineTestCase):
         # "publish" clause is ignored in case of ERROR so task execution field
         # must be empty.
         self.assertDictEqual({}, task1.published)
+        self.assertEqual(2, data_flow.get_task_execution_result(task1))
+
+    def test_error_result2(self):
+        wf_service.create_workflows(WF)
+
+        # Start workflow.
+        wf_ex = self.engine.start_workflow(
+            'wf',
+            {
+                'success_result': None,
+                'error_result': 3
+            }
+        )
+
+        self._await(lambda: self.is_execution_success(wf_ex.id))
+
+        # Note: We need to reread execution to access related tasks.
+        wf_ex = db_api.get_workflow_execution(wf_ex.id)
+
+        tasks = wf_ex.task_executions
+
+        self.assertEqual(2, len(tasks))
+
+        task1 = self._assert_single_item(tasks, name='task1')
+        task3 = self._assert_single_item(tasks, name='task3')
+
+        self.assertEqual(states.ERROR, task1.state)
+        self.assertEqual(states.SUCCESS, task3.state)
+
+        # "publish" clause is ignored in case of ERROR so task execution field
+        # must be empty.
+        self.assertDictEqual({}, task1.published)
+        self.assertEqual(3, data_flow.get_task_execution_result(task1))
+
+    def test_success_result(self):
+        wf_service.create_workflows(WF)
+
+        # Start workflow.
+        wf_ex = self.engine.start_workflow(
+            'wf',
+            {
+                'success_result': 'success',
+                'error_result': None
+            }
+        )
+
+        self._await(lambda: self.is_execution_success(wf_ex.id))
+
+        # Note: We need to reread execution to access related tasks.
+        wf_ex = db_api.get_workflow_execution(wf_ex.id)
+
+        tasks = wf_ex.task_executions
+
+        self.assertEqual(1, len(tasks))
+
+        task1 = self._assert_single_item(tasks, name='task1')
+
+        self.assertEqual(states.SUCCESS, task1.state)
+
+        # "publish" clause is ignored in case of ERROR so task execution field
+        # must be empty.
+        self.assertDictEqual({'p_var': 'success'}, task1.published)
+        self.assertEqual('success', data_flow.get_task_execution_result(task1))
