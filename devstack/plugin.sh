31ee23296a0918e725bbd4709dd154401eb40112
Oleksii Chuprykov <ochuprykov@mirantis.com>
2015-10-06 14:25:32 +0300

    Colorise mistral log output
    This makes debugging more funny.
    
    Change-Id: Ib5b20c8bb9b4acd331c87f1f8224cf053401d65b
    (cherry picked from commit a51ef9c0b94973411d786cb4b66dd0b072379463)

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -101,6 +101,10 @@ function configure_mistral {
 
     # Configure action execution deletion policy
     iniset $MISTRAL_CONF_FILE api allow_action_execution_deletion True
+
+    if [ "$LOG_COLOR" == "True" ] && [ "$SYSLOG" == "False" ]; then
+        setup_colorized_logging $MISTRAL_CONF_FILE DEFAULT tenant user
+    fi
 }
 
 
