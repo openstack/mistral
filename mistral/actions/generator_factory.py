bf7728743c2879f06dab66c90d83278cfef887e4
Lingxian Kong <anlin.kong@gmail.com>
2015-12-04 17:53:55 +0800

    Refactor action generator
    When contributor wants to support new OpenStack service actions in
    Mistral, there is no need to add different ActionGenerator classes
    respectively and its corresponding unit tests.
    
    Change-Id: Ifd49087c84164ecebfa095a9c6c85c089d807a8c

--- mistral/actions/generator_factory.py
+++ mistral/actions/generator_factory.py
@@ -1,6 +1,6 @@
 # Copyright 2014 - Mirantis, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+#    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
 #
@@ -12,18 +12,29 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from mistral.actions.openstack.action_generator import generators
+from oslo_utils import importutils
+
+from mistral.actions.openstack.action_generator import base
+
+
+SUPPORTED_MODULES = [
+    'Nova', 'Glance', 'Keystone', 'Heat', 'Neutron', 'Cinder', 'Ceilometer',
+    'Trove', 'Ironic'
+]
 
 
 def all_generators():
-    return [
-        generators.NovaActionGenerator,
-        generators.GlanceActionGenerator,
-        generators.KeystoneActionGenerator,
-        generators.HeatActionGenerator,
-        generators.NeutronActionGenerator,
-        generators.CinderActionGenerator,
-        generators.CeilometerActionGenerator,
-        generators.TroveActionGenerator,
-        generators.IronicActionGenerator
-    ]
+    for mod_name in SUPPORTED_MODULES:
+        mod_namespace = mod_name.lower()
+        mod_cls_name = 'mistral.actions.openstack.actions.%sAction' % mod_name
+        mod_action_cls = importutils.import_class(mod_cls_name)
+        generator_cls_name = '%sActionGenerator' % mod_name
+
+        yield type(
+            generator_cls_name,
+            (base.OpenStackActionGenerator,),
+            {
+                'action_namespace': mod_namespace,
+                'base_action_class': mod_action_cls
+            }
+        )
