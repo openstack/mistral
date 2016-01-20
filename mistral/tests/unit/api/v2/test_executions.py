eff2a85ef14508519c8f80a5f5a6b83680fdb5b2
Winson Chan <wcchan@stackstorm.com>
2016-01-20 01:41:16 +0000

    Add task_execution_id to workflow execution in API
    The task_execution_id is required to identify whether the workflow execution
    is a sub workflow.
    
    Change-Id: I85f83873ac395f08fd97ae2b04fa5e5cc6cbcb3d
    Closes-Bug: #1535967

--- mistral/tests/unit/api/v2/test_executions.py
+++ mistral/tests/unit/api/v2/test_executions.py
@@ -18,6 +18,7 @@ import copy
 import datetime
 import json
 import mock
+import uuid
 from webtest import app as webtest_app
 
 from mistral.db.v2 import api as db_api
@@ -55,6 +56,35 @@ WF_EX_JSON = {
     'workflow_name': 'some',
 }
 
+SUB_WF_EX = models.WorkflowExecution(
+    id=str(uuid.uuid4()),
+    workflow_name='some',
+    description='foobar',
+    spec={'name': 'some'},
+    state=states.RUNNING,
+    state_info=None,
+    input={'foo': 'bar'},
+    output={},
+    params={'env': {'k1': 'abc'}},
+    created_at=datetime.datetime(1970, 1, 1),
+    updated_at=datetime.datetime(1970, 1, 1),
+    task_execution_id=str(uuid.uuid4())
+)
+
+SUB_WF_EX_JSON = {
+    'id': SUB_WF_EX.id,
+    'workflow_name': 'some',
+    'description': 'foobar',
+    'input': '{"foo": "bar"}',
+    'output': '{}',
+    'params': '{"env": {"k1": "abc"}}',
+    'state': 'RUNNING',
+    'state_info': None,
+    'created_at': '1970-01-01 00:00:00',
+    'updated_at': '1970-01-01 00:00:00',
+    'task_execution_id': SUB_WF_EX.task_execution_id
+}
+
 UPDATED_WF_EX = copy.deepcopy(WF_EX)
 UPDATED_WF_EX['state'] = states.PAUSED
 
@@ -72,6 +102,7 @@ WF_EX_JSON_WITH_DESC = copy.deepcopy(WF_EX_JSON)
 WF_EX_JSON_WITH_DESC['description'] = "execution description."
 
 MOCK_WF_EX = mock.MagicMock(return_value=WF_EX)
+MOCK_SUB_WF_EX = mock.MagicMock(return_value=SUB_WF_EX)
 MOCK_WF_EXECUTIONS = mock.MagicMock(return_value=[WF_EX])
 MOCK_UPDATED_WF_EX = mock.MagicMock(return_value=UPDATED_WF_EX)
 MOCK_DELETE = mock.MagicMock(return_value=None)
@@ -89,6 +120,15 @@ class TestExecutionsController(base.FunctionalTest):
 
         self.assertEqual(200, resp.status_int)
         self.assertDictEqual(WF_EX_JSON_WITH_DESC, resp.json)
+
+    @mock.patch.object(db_api, 'get_workflow_execution', MOCK_SUB_WF_EX)
+    def test_get_sub_wf_ex(self):
+        resp = self.app.get('/v2/executions/123')
+
+        self.maxDiff = None
+
+        self.assertEqual(200, resp.status_int)
+        self.assertDictEqual(SUB_WF_EX_JSON, resp.json)
 
     @mock.patch.object(db_api, 'get_workflow_execution', MOCK_NOT_FOUND)
     def test_get_not_found(self):
