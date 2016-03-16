2a786a7a9b82708ff80083b38f97c2d815e9cc4a
Thomas Herve <therve@redhat.com>
2016-03-16 10:43:21 +0100

    Fix compatibility with WebOb 1.6.0
    THe new WebOb release got stricter on the way Response text can be
    retrieved for JSON responses. This fixes the issue by checking the body
    instead in tests.
    
    Change-Id: I45670bbebda9220ff3e69f10590d20dbc605f4a7
    (cherry picked from commit cc45eeaf818b2ffe13915e322b28476b6fa922bc)

--- mistral/tests/unit/api/v2/test_workflows.py
+++ mistral/tests/unit/api/v2/test_workflows.py
@@ -200,7 +200,10 @@ class TestWorkflowsController(base.FunctionalTest):
         )
 
         self.assertEqual(400, resp.status_int)
-        self.assertIn("Attempt to modify a system workflow", resp.text)
+        self.assertIn(
+            "Attempt to modify a system workflow",
+            resp.body.decode()
+        )
 
     @mock.patch.object(db_api, "update_workflow_definition")
     def test_put_public(self, mock_update):
@@ -303,7 +306,7 @@ class TestWorkflowsController(base.FunctionalTest):
         )
 
         self.assertEqual(400, resp.status_int)
-        self.assertIn("Scope must be one of the following", resp.text)
+        self.assertIn("Scope must be one of the following", resp.body.decode())
 
     @mock.patch.object(db_api, "create_workflow_definition", MOCK_DUPLICATE)
     def test_post_dup(self):
@@ -339,7 +342,10 @@ class TestWorkflowsController(base.FunctionalTest):
         resp = self.app.delete('/v2/workflows/123', expect_errors=True)
 
         self.assertEqual(400, resp.status_int)
-        self.assertIn("Attempt to delete a system workflow", resp.text)
+        self.assertIn(
+            "Attempt to delete a system workflow",
+            resp.body.decode()
+        )
 
     @mock.patch.object(db_api, "delete_workflow_definition", MOCK_NOT_FOUND)
     def test_delete_not_found(self):
