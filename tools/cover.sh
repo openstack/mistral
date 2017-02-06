2abe89288476ff5c58218327cc1b115fda909530
Sharat Sharma <sharat.sharma@nectechnologies.in>
2017-02-06 12:57:58 +0530

    Remove wrong licensing
    This file is wrongly licensed and copyrighted. So, removing it.
    
    Change-Id: I1fcd6837f8c3dc432f76f1457859800c23117eeb
    (cherry picked from commit c374d9852616a204db1e39f7fe2c3b22e89dbc99)

--- tools/cover.sh
+++ tools/cover.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #
-# Copyright 2015: Mirantis Inc.
-# All Rights Reserved.
-#
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
