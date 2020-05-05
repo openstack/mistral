84041d67b4c32ec74d270216e6cc0be4428f8291
Corey Bryant <corey.bryant@canonical.com>
2020-05-05 15:55:12 -0400

    Monkey patch original current_thread _active
    Monkey patch the original current_thread to use the up-to-date _active
    global variable. This solution is based on that documented at:
    https://github.com/eventlet/eventlet/issues/592
    
    Change-Id: I98e80bcbc7637bbb6063935043e530718be87f7b
    Closes-Bug: #1863021

--- mistral/cmd/launch.py
+++ mistral/cmd/launch.py
@@ -25,6 +25,12 @@ eventlet.monkey_patch(
     socket=True,
     thread=False if '--use-debugger' in sys.argv else True,
     time=True)
+# Monkey patch the original current_thread to use the up-to-date _active
+# global variable. See https://bugs.launchpad.net/bugs/1863021 and
+# https://github.com/eventlet/eventlet/issues/592
+import __original_module_threading as orig_threading
+import threading  # noqa
+orig_threading.current_thread.__globals__['_active'] = threading._active
 
 
 import os
