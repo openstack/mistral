dab6774d5c0ab5fa59264dc62380fc411703c3b8
Swapnil Vaidya <vaidyaswapneel@gmail.com>
2025-11-13 18:44:31 +0500

    changes for sec

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 
 setuptools.setup(
     setup_requires=[
-        'setuptools==78.1.1',
+        'setuptools==80.9.0',
         'pbr>=2.0.0'
     ],
     pbr=True
--- tests/robot/setup.py
+++ tests/robot/setup.py
@@ -23,6 +23,6 @@ setup(
         'Programming Language:: Python:: 3.6'
     ],
     setup_requires=[
-        'setuptools==78.1.1'
+        'setuptools==80.9.0'
     ],
 )
